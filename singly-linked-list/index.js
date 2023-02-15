bc75d3135f271344ddc3034c17d8fe7e4fc4bf7f
alantmatos <mr.alanmatos@hotmail.com>
2023-02-14 22:18:52 -0500

    linked-list shift method

--- singly-linked-list/index.js
+++ singly-linked-list/index.js
@@ -49,6 +49,17 @@ class Node{
       return current
     }
 
+    shift(){
+      if(!this.head) return undefined;
+      let currentHead = this.head;
+      this.head = currentHead.next;
+      this.length --;
+          if(this.length === 0){
+        this.tail = null;
+      }
+      return currentHead;
+    }
+
   
     
     
