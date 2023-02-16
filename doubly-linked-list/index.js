e0c40c98c738beb7a31fa6a83aefe4b8fce9e333
alantmatos <mr.alanmatos@hotmail.com>
2023-02-16 10:12:54 -0500

    merge sort

--- doubly-linked-list/index.js
+++ doubly-linked-list/index.js
@@ -3,3 +3,15 @@
 ///////////////////////////////////////////
 /////////// Doubly Linked List ///////////
 /////////////////////////////////////////
+
+function mergeSort(arr) {
+  // Base case
+  if (arr.length <= 1) return arr
+  let mid = Math.floor(arr.length / 2)
+  // Recursive calls
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+  return merge(left, right)
+}
+
+mergeSort([1,2,4,5])
