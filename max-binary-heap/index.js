0bcf634fc7a772e10951752b87b4ba42d4cb8346
alantmatos <mr.alanmatos@hotmail.com>
2023-02-28 14:41:31 -0500

    max-binary-heap, extract maximum value/remove method

--- max-binary-heap/index.js
+++ max-binary-heap/index.js
@@ -26,4 +26,36 @@ class MaxBinaryHeap {
             idx = parentIdx;
         }
     }
+
+
+    extractMax() {
+        if (!this.values.length) return;
+
+        this.swap(0, this.values.length - 1);
+        const oldNode = this.values.pop();
+
+        //trikle down
+        let parent = 0, childLeft = 1, childRight = 2;
+        //Math.max returns NaN is one of the arguments is undefined
+        let max = Math.max(this.values[childLeft], this.values[childRight] || -Infinity);
+
+        while (this.values[parent] < max) {
+
+            let child = this.values[childLeft] === max ? childLeft : childRight;
+            this.swap(parent, child);
+            parent = child;
+
+            childLeft = parent * 2 + 1;
+            childRight = parent * 2 + 2;
+            max = Math.max(this.values[childLeft], this.values[childRight] || -Infinity);
+        }
+        return oldNode;
+    }
+    
+    swap(inx1, inx2) {
+        [this.values[inx1], this.values[inx2]] = [this.values[inx2], this.values[inx1]];
+    }
+
+
+
 }
