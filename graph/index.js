5dd56f1add15b994ea517a2a619183e3ce1ff83c
alantmatos <mr.alanmatos@hotmail.com>
2023-03-02 12:46:36 -0500

    add edge method

---
+++ graph/index.js
@@ -0,0 +1,28 @@
+
+///////////////////////////////////
+
+
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(data) {
+    if (!this.adjacencyList[data]) {
+      this.adjacencyList[data] = []
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1]) {
+      this.adjacencyList[vertex1].push(vertex2);
+    }
+    if (this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
+
+
+
+
+}
